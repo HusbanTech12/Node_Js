9cf3cd64bc8b0f4ae9c57710fd2010e39bce89c9
HusbanTech12 <husbantech08@gmail.com>
2025-07-08 22:10:02 +0500

    coreModule

--- FirstProgram.js
+++ FirstProgram.js
@@ -1,5 +1,10 @@
 // console.log('Husban');
 
+// node.js lecture #1
+
+//  1) node.js runh time server environment hai.
+// 2) console.log() javaScript for web broswer hota hai and Node.js mn for server side or module hota h.
+ 
 
 const a = 10;
 
---
+++ coreModule.js
@@ -0,0 +1,20 @@
+// Gobal Core Module : They are Modules that Can use without import.
+// Non-Global Core Module : They are Modules that Can use with import. 
+
+// Global Core Module Exp:
+
+// console.log() use without import
+console.log('Husban');
+
+
+
+// Non-Global Core Module Exp:
+
+// Create instance myFile and import 'fs' module
+const myFile = require('fs')
+console.log('Node Js Core Module');
+
+// writeFileSync : create file  
+
+//                    your file name ,  what write in file     
+myFile.writeFileSync('coreModule.txt','Global and Non-Global Core Module')
